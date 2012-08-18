1ab5bb3748518d81bd8978a053ee11e15a8dadc5
Martin Owens <doctormo@gmail.com>
2012-08-18 01:22:45 -0400

    Add usage and improve compat testing

---
+++ tests/test_usage.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2012 Jay Sigbrandt <jsigbrandt@slb.com>
+#                    Martin Owens <doctormo@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+Test crontab usage.
+"""
+
+import os
+import sys
+
+sys.path.insert(0, '../')
+
+import unittest
+from test import test_support
+from crontab import CronTab, EXAMPLE_USE
+
+class DummyStdout(object):
+    def write(self, text):
+        pass
+
+class UseTestCase(unittest.TestCase):
+    """Test use documentation in crontab."""
+    def test_03_usage(self):
+        """Dont modify crontab"""
+        cron = CronTab()
+        original = cron.render()
+        sys.stdout = DummyStdout()
+        exec(EXAMPLE_USE)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(cron.render(), original)
+
+if __name__ == '__main__':
+    test_support.run_unittest(
+       UseTestCase,
+    )
